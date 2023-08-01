b645c59fccbea21d882788a75eb5c5f7d091bbb9
이지호 <search5@mz.co.kr>
2023-08-01 22:56:21 +0900

    BS5에 맞게 템플릿 수정중

--- portia_web/src/components/TopMenuView.vue
+++ portia_web/src/components/TopMenuView.vue
@@ -7,42 +7,20 @@ export default defineComponent({
 </script>
 
 <template>
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="/">KNOU 쇼핑몰</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="/">처음 화면
-            <span class="sr-only">(current)</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/goods">상품 목록</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/join">회원 가입</a>
-        </li>
-        {% if not session.email %}
-        <li class="nav-item">
-            <a class="nav-link" href="/login">로그인</a>
-        </li>
-        {% else %}
-        <li class="nav-item">
-            <a class="nav-link" href="/logout">로그아웃</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/basket">장바구니</a>
-        </li>
-        {% endif %}
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <img src="@/assets/logo.png" class="img-fluid" style="height: 40px;">
+      <span class="fs-4">Portia Shop</span>
+    </a>
+
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">처음 화면</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">상품 목록</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">회원 가입</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">로그인</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">장바구니</a></li>
+    </ul>
+  </header>
 </template>
 
 <style lang="scss" scoped>
--- portia_web/src/views/IndexView.vue
+++ portia_web/src/views/IndexView.vue
@@ -12,18 +12,10 @@ export default defineComponent({
 <template>
 
 <div>
-<!--{% block css %}-->
-<!-- Custom styles for this template -->
-<!--<link href="/static/css/shop-homepage.css" rel="stylesheet">-->
-<!--{% endblock %}-->
-
   <TopMenuView></TopMenuView>
-  <button class="btn btn-primary">Hello Jiho</button>
 
-<!--{% block body %}-->
   <!-- Page Content -->
   <div class="container">
-
 <!--    {% with messages = get_flashed_messages() %}-->
 <!--      {% if messages %}-->
 <!--      <div class="alert alert-danger mt-3" role="alert"></div>-->
@@ -36,100 +28,98 @@ export default defineComponent({
 <!--      {% endif %}-->
 <!--    {% endwith %}-->
 
-<!--    <div class="row">-->
-
-<!--      <div class="col-lg-3">-->
-<!--        <h1 class="my-4">KNOU 쇼핑몰</h1>-->
+    <div class="row">
+      <div class="col-lg-3">
+        <h1 class="my-4">Portia 쇼핑몰</h1>
 <!--        {% if not session.email %}-->
-<!--        <form method="post" action="/login">-->
-<!--            <div class="form-group row">-->
-<!--              <label for="inputEmail3" class="col-sm-4 col-form-label">이메일</label>-->
-<!--              <div class="col-sm-8">-->
-<!--                <input type="email" class="form-control form-control-sm" id="inputEmail3" name="email">-->
-<!--              </div>-->
-<!--            </div>-->
-<!--            <div class="form-group row">-->
-<!--              <label for="inputPassword3" class="col-sm-4 col-form-label">비밀번호</label>-->
-<!--              <div class="col-sm-8">-->
-<!--                <input type="password" class="form-control form-control-sm" id="inputPassword3" name="password">-->
-<!--              </div>-->
-<!--            </div>-->
-<!--            <div class="form-group row">-->
-<!--              <div class="col-sm-10 text-right">-->
-<!--                <button type="submit" class="btn btn-success">로그인</button>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </form>-->
+        <form method="post" action="/login">
+            <div class="form-group row">
+              <label for="inputEmail3" class="col-sm-4 col-form-label">이메일</label>
+              <div class="col-sm-8">
+                <input type="email" class="form-control form-control-sm" id="inputEmail3" name="email">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="inputPassword3" class="col-sm-4 col-form-label">비밀번호</label>
+              <div class="col-sm-8">
+                <input type="password" class="form-control form-control-sm" id="inputPassword3" name="password">
+              </div>
+            </div>
+            <div class="form-group row">
+              <div class="col-sm-10 text-right">
+                <button type="submit" class="btn btn-success">로그인</button>
+              </div>
+            </div>
+          </form>
 <!--          {% else %}-->
 <!--            {{ session['email'] }}님 로그인하셨습니다.-->
 <!--          {% endif %}-->
-<!--      </div>-->
-<!--      &lt;!&ndash; /.col-lg-3 &ndash;&gt;-->
+      </div>
+      <!-- /.col-lg-3 -->
 
-<!--      <div class="col-lg-9">-->
-<!--        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">-->
-<!--          <ol class="carousel-indicators">-->
-<!--            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>-->
-<!--            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
-<!--            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
-<!--          </ol>-->
-<!--          <div class="carousel-inner" role="listbox">-->
-<!--            <div class="carousel-item active">-->
-<!--              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">-->
-<!--            </div>-->
-<!--            <div class="carousel-item">-->
-<!--              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">-->
-<!--            </div>-->
-<!--            <div class="carousel-item">-->
-<!--              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">-->
-<!--            </div>-->
-<!--          </div>-->
-<!--          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">-->
-<!--            <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-<!--            <span class="sr-only">Previous</span>-->
-<!--          </a>-->
-<!--          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">-->
-<!--            <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-<!--            <span class="sr-only">Next</span>-->
-<!--          </a>-->
-<!--        </div>-->
-
-<!--        <div class="row">-->
-
+      <div class="col-lg-9">
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+<!---->
+        <div class="row">
+<!---->
 <!--          {% for item in goods %}-->
-<!--          <div class="col-lg-4 col-md-6 mb-4">-->
-<!--            <div class="card h-100">-->
-<!--              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
-<!--              <div class="card-body">-->
-<!--                <h4 class="card-title">-->
-<!--                  <a href="/goods/{{ item.id }}">{{ item.goods_name }} </a>-->
-<!--                </h4>-->
-<!--                <h5>{{ '%d' | format(item.price) }}</h5>-->
-<!--                <p class="card-text">{{ item.goods_description }}</p>-->
-<!--              </div>-->
-<!--              <div class="card-footer">-->
-<!--                <small class="text-muted">-->
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="/goods/{{ item.id }}">{{ item.goods_name }} </a>
+                </h4>
+                <h5>{{ '%d' | format(item.price) }}</h5>
+                <p class="card-text">{{ item.goods_description }}</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">
 <!--                  {% for item in range(item.goods_ranking) %}-->
 <!--                  &#9733;-->
 <!--                  {% endfor %}-->
-
+<!---->
 <!--                  {% for item in range(5 - item.goods_ranking) %}-->
 <!--                    &#9734;-->
 <!--                  {% endfor %}-->
-<!--                </small>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
+                </small>
+              </div>
+            </div>
+          </div>
 <!--          {% endfor %}-->
-<!--        </div>-->
-<!--        &lt;!&ndash; /.row &ndash;&gt;-->
+        </div>
+        <!-- /.row -->
 
-<!--      </div>-->
+      </div>
       <!-- /.col-lg-9 -->
 
-<!--    </div>-->
+    </div>
     <!-- /.row -->
-111
   </div>
   <!-- /.container -->
 

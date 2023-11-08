a364f57e0375a41cc4f411339d62c26a661706ae
Ji-Ho Lee <search5@gmail.com>
2023-11-08 12:44:56 +0900

    테스트 stub 작성(구현중 추가로 필요할 수 있음)

---
+++ tests/test_myinfo.py
@@ -0,0 +1,13 @@
+def test_myinfo_get_success(client):
+    res = 500
+    assert res == 200, "Not Implemented"
+
+
+def test_myinfo_modify_success(client):
+    res = 500
+    assert res == 200, "Not Implemented"
+
+
+def test_myinfo_modify_failure_badinfo(client):
+    res = 500
+    assert res == 200, "Not Implemented"

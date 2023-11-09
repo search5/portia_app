63359f5c41eb6a8df9c2a9d824b4a33756c86cf9
이지호 <search5@mz.co.kr>
2023-11-09 20:56:10 +0900

    ..

--- portia/tests/test_cart.py
+++ portia/tests/test_cart.py
@@ -4,26 +4,35 @@
 - [ ] 장바구니 수정 기능
 - [ ] 장바구니 지우기 기능
 """
+
+
 def test_cart_listing_success(client):
     pass
+
 
 def test_cart_listing_failure(client):
     pass
 
+
 def test_cart_add_success(client):
     pass
+
 
 def test_cart_add_failure(client):
     pass
 
+
 def test_cart_modify_success(client):
     pass
+
 
 def test_cart_modify_failure(client):
     pass
 
+
 def test_cart_delete_success(client):
     pass
+
 
 def test_cart_delete_failure(client):
     pass

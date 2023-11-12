fa5c81b3c7307aa03a339c9565780bc2c0eddcf3
Ji-Ho Lee <search5@gmail.com>
2023-11-12 16:12:51 +0900

    View 파일 추가 중

--- portia_web/src/views/OrdersView.vue
+++ portia_web/src/views/OrdersView.vue
@@ -1,11 +1,22 @@
 <script>
+import TopMenuView from "@/components/TopMenuView.vue";
+import FooterView from "@/components/FooterView.vue";
+
 export default {
-  name: "OrdersView"
+  name: "OrdersView",
+  components: {FooterView, TopMenuView}
 }
 </script>
 
 <template>
+  <div>
+    <TopMenuView></TopMenuView>
 
+    <div class="container mb-3">
+    </div>
+
+    <FooterView></FooterView>
+  </div>
 </template>
 
 <style scoped>
